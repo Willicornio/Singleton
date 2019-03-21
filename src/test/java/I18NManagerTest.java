651368b618c609d86ef3d32e5602d96f58742831
Willicornio <guillecacerola@gmail.com>
2019-03-21 19:18:46 +0100

    Singleton basico con test

---
+++ src/test/java/I18NManagerTest.java
@@ -0,0 +1,13 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class I18NManagerTest {
+
+    @Test
+    public void test() {
+        String r = I18NManager.getInstance().getEtiqueta("en","l1");
+        Assert.assertEquals("Hello",r);
+
+    }
+}
